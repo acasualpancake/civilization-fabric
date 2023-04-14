6ad2e708ede24188730d8b64edeeb33e1481e535
LostBoyHubble <malte-grossmann@gmx.de>
2023-04-14 15:43:45 +0200

    Added discord bot integration (not functional yet)

---
+++ src/main/java/net/acasualpancake/discord/BotUtil.java
@@ -0,0 +1,13 @@
+package net.acasualpancake.discord;
+
+@SuppressWarnings("unused")
+public class BotUtil {
+
+    /**
+     * <p>Utility class.</p>
+     */
+    private BotUtil() {}
+
+    public static void runBot() {
+    }
+}
