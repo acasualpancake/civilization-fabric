127ed18c9d3e31ef850ee0a4051a9d26b970b09a
LostBoyHubble <malte-grossmann@gmx.de>
2023-04-14 16:00:26 +0200

    Added bot utilities to start and initialize the bot

--- src/main/java/net/acasualpancake/discord/BotUtil.java
+++ src/main/java/net/acasualpancake/discord/BotUtil.java
@@ -1,13 +1,46 @@
 package net.acasualpancake.discord;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
 @SuppressWarnings("unused")
 public class BotUtil {
+
+    public static JDA JDA;
 
     /**
      * <p>Utility class.</p>
      */
     private BotUtil() {}
 
-    public static void runBot() {
+    /**
+     * <p>Starts the discord bot and automatically initializes it.</p>
+     * @param token The bot token.
+     */
+    public static void runBot(String token) {
+        JDABuilder builder = JDABuilder.createDefault(token);
+        builder.enableIntents(
+                GatewayIntent.GUILD_MEMBERS,
+                GatewayIntent.GUILD_INVITES,
+                GatewayIntent.GUILD_EMOJIS_AND_STICKERS,
+                GatewayIntent.GUILD_MESSAGES,
+                GatewayIntent.GUILD_MESSAGE_REACTIONS,
+                GatewayIntent.GUILD_PRESENCES
+        );
+        builder.setActivity(Activity.listening("Startup"));
+        builder.setStatus(OnlineStatus.DO_NOT_DISTURB);
+        init(builder);
+        JDA = builder.build();
+    }
+
+    /**
+     * <p>Please use this method to register events, etc.</p>
+     * @param builder The active discord jda builder.
+     */
+    public static void init(JDABuilder builder) {
+        // Do code here
     }
 }
